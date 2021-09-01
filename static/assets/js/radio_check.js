58bc010a821f3048cd97d995a1b853e5b04b7f14
Adensty7 <78910585+Adensty7@users.noreply.github.com>
2021-09-01 18:30:31 +0530

    Added Onclick option in checkboxes

--- static/assets/js/radio_check.js
+++ static/assets/js/radio_check.js
@@ -2,3 +2,13 @@ function check(id) {
     const radio = document.querySelector(id)
     radio.checked = true;
   }
+
+function checkbox_check(id) {
+  const checkbox = document.querySelector(id)
+  if(checkbox.checked === true) {
+    checkbox.checked = false;
+  }
+  else {
+    checkbox.checked = true;
+  }
+}
