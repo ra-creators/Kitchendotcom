470cbf39fba049f3f03748485bf159eabe07d0d1
abdev <ab.inpathtoadev@gmail.com>
2021-09-14 17:48:16 +0530

    frontend integration init

---
+++ static/projectgallery/js/jquery.hislide.js
@@ -0,0 +1,66 @@
+(function($) {
+    var slide = function(ele,options) {
+        var $ele = $(ele);
+        var setting = {
+            speed: 1000,
+            interval:2000,
+            
+        };
+        $.extend(true, setting, options);
+        var states = [
+            { $zIndex: 1, width: 190, height: 235, top: 59, left: 100, $opacity: 0.4 },
+            { $zIndex: 2, width: 240, height: 285, top: 35, left: 290, $opacity: 0.7 },
+            { $zIndex: 3, width: 320, height: 345, top: 0, left: 520, $opacity: 1 },
+            { $zIndex: 2, width: 240, height: 285, top: 35, left: 830, $opacity: 0.7 },
+            { $zIndex: 1, width: 190, height: 235, top: 59, left: 1060, $opacity: 0.4 },
+        ];
+
+        var $lis = $ele.find('li');
+        var timer = null;
+
+        
+        $ele.find('.hi-next').on('click', function() {
+            next();
+        });
+        $ele.find('.hi-prev').on('click', function() {
+            states.push(states.shift());
+            move();
+        });
+        $ele.on('mouseenter', function() {
+            clearInterval(timer);
+            timer = null;
+        }).on('mouseleave', function() {
+            autoPlay();
+        });
+
+        move();
+        autoPlay();
+
+       
+        function move() {
+            $lis.each(function(index, element) {
+                var state = states[index];
+                $(element).css('zIndex', state.$zIndex).finish().animate(state, setting.speed).find('img').css('opacity', state.$opacity);
+            });
+        }
+
+       
+        function next() {
+            
+            states.unshift(states.pop());
+            move();
+        }
+
+        function autoPlay() {
+            timer = setInterval(next, setting.interval);
+        }
+    }
+    
+    $.fn.hiSlide = function(options) {
+        $(this).each(function(index, ele) {
+            slide(ele,options);
+        });
+       
+        return this;
+    }
+})(jQuery);
