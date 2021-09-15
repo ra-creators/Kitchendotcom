8934d74ada782fdb71727eaa121fbf8eb1b3ff32
abdev <ab.inpathtoadev@gmail.com>
2021-09-15 19:00:45 +0530

    heroku #04

--- Kitchendotcom/settings.py
+++ Kitchendotcom/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
-
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -133,3 +133,6 @@ STATICFILES_DIRS = [
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+
