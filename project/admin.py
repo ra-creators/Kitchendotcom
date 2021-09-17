224eb2057e48b652d5705242f519fd56679c20d5
Pratham Khandelwal <kwalpratham22@gmail.com>
2021-09-17 23:21:56 +0530

    Registered models in project

--- project/admin.py
+++ project/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from project.models import Project
+from project.models import Project, Video, Feedback, Design
 
+admin.site.register(Video)
+admin.site.register(Design)
+admin.site.register(Feedback)
 admin.site.register(Project)
+
