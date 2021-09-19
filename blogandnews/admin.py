da535a3233896d59286a0605d2dcf470871600c6
Pratham Khandelwal <kwalpratham22@gmail.com>
2021-09-19 19:52:44 +0530

    Added models for BlogandNews

--- blogandnews/admin.py
+++ blogandnews/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
+from blogandnews.models import Blog, News
+
+admin.site.register(Blog)
+admin.site.register(News)
--- blogandnews/models.py
+++ blogandnews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    Coverimage = models.FileField(upload_to="")
+    Author = models.CharField()
+    Content = models.TextField()
+
+class News(models.Model):
+    Coverimage = models.FileField(upload_to="")
+    Author = models.CharField()
+    Content = models.TextField()
