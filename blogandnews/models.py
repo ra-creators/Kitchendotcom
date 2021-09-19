3c4dff0f8c586c2d0f5bf14ae7325070fcc0fdcd
abdev <ab.inpathtoadev@gmail.com>
2021-09-19 15:54:42 +0530

    blog&news

--- blogandnews/models.py
+++ blogandnews/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=100, default="Title")
+    author = models.CharField(max_length=100, default="admin")
+    date = models.DateField(auto_now=True)
+    image = models.FileField(upload_to="blogs/", max_length=100)
+    content = models.TextField(default="NA")
+
+    def __str__(self):
+        return self.title
+
+
+class News(models.Model):
+    heading = models.CharField(max_length=100, default="Title")
+    author = models.CharField(max_length=100, default="admin")
+    date = models.DateField(auto_now=True)
+    image = models.FileField(upload_to="blogs/", max_length=100)
+    content = models.TextField(max_length=1000, default="NA")
+
+    def __str__(self):
+        return self.heading
+
--- blogandnews/urls.py
+++ blogandnews/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.blogandnews, name="blogandnews"),
     path('blog/<int:blogId>', views.blog, name="blog"),
-    path('news/<int:newsId>', views.news, name="news"),
 
 ]
