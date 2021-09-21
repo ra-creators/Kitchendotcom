142a4895b8f4b9baaa67b5211cd53632d62e8b5d
abdev <ab.inpathtoadev@gmail.com>
2021-09-21 19:18:04 +0530

    nav added

--- static/projectgallery/js/jquery.hislide.js
+++ static/projectgallery/js/jquery.hislide.js
@@ -1,52 +1,76 @@
-(function($) {
-    var slide = function(ele,options) {
+(function ($) {
+    var slide = function (ele, options) {
         var $ele = $(ele);
         var setting = {
             speed: 1000,
-            interval:2000,
-            
+            interval: 2000,
+
         };
         $.extend(true, setting, options);
-        var states = [
-            { $zIndex: 1, height: 225, width: 400, top: 59, left: 100, $opacity: 0.4 },
-            { $zIndex: 2, height: 270, width: 480, top: 35, left: 290, $opacity: 0.7 },
-            { $zIndex: 3, height: 297, width: 528, top: 0, left: 520, $opacity: 1 },
-            { $zIndex: 2, height: 270, width: 480, top: 35, left: 830, $opacity: 0.7 },
-            { $zIndex: 1, height: 225, width: 400, top: 59, left: 1060, $opacity: 0.4 },
+        var states = [{
+                $zIndex: 1,
+                height: 225,
+                width: 400,
+                $opacity: 0.4
+            },
+            {
+                $zIndex: 2,
+                height: 270,
+                width: 480,
+                $opacity: 0.7
+            },
+            {
+                $zIndex: 3,
+                height: 297,
+                width: 528,
+                $opacity: 1
+            },
+            {
+                $zIndex: 2,
+                height: 270,
+                width: 480,
+                $opacity: 0.7
+            },
+            {
+                $zIndex: 1,
+                height: 225,
+                width: 400,
+                $opacity: 0.4
+            },
         ];
 
         var $lis = $ele.find('li');
         var timer = null;
 
-        
-        $ele.find('.hi-next').on('click', function() {
+
+        $ele.find('.hi-next').on('click', function () {
             next();
         });
-        $ele.find('.hi-prev').on('click', function() {
+        $ele.find('.hi-prev').on('click', function () {
             states.push(states.shift());
             move();
         });
-        $ele.on('mouseenter', function() {
+        $ele.on('mouseenter', function () {
             clearInterval(timer);
             timer = null;
-        }).on('mouseleave', function() {
+        }).on('mouseleave', function () {
             autoPlay();
         });
 
         move();
         autoPlay();
 
-       
+
         function move() {
-            $lis.each(function(index, element) {
+            $lis.each(function (index, element) {
                 var state = states[index];
                 $(element).css('zIndex', state.$zIndex).finish().animate(state, setting.speed).find('img').css('opacity', state.$opacity);
             });
         }
 
-       
+
         function next() {
-            
+
             states.unshift(states.pop());
             move();
         }
@@ -55,12 +79,12 @@
             timer = setInterval(next, setting.interval);
         }
     }
-    
-    $.fn.hiSlide = function(options) {
-        $(this).each(function(index, ele) {
-            slide(ele,options);
+
+    $.fn.hiSlide = function (options) {
+        $(this).each(function (index, ele) {
+            slide(ele, options);
         });
-       
+
         return this;
     }
 })(jQuery);
