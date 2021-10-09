32565902f68325b1f6f25b033a2b6bdba4d461ef
abhakta-47 <ab.inpathtoadev@gmail.com>
2021-10-09 22:03:43 +0530

    blog comment added

--- blogandnews/views.py
+++ blogandnews/views.py
@@ -1,29 +1,31 @@
-from django.shortcuts import get_object_or_404, render
+from django.shortcuts import get_object_or_404, render, redirect
 from .models import Blog, News, BlogComment
 # Create your views here.
+
+
 def blogandnews(request):
-    blogs = Blog.objects.all() 
-    newss = News.objects.all() 
-    return render(request, "blogandnews.html",{
-        'blogs':blogs,
-        'newss':newss,
+    blogs = Blog.objects.all()
+    newss = News.objects.all()
+    return render(request, "blogandnews.html", {
+        'blogs': blogs,
+        'newss': newss,
     })
+
 
 def blog(request, blogId):
     blog = get_object_or_404(Blog, id=blogId)
     comments = BlogComment.objects.filter(blog=blog)
-    context = {'blog' : blog, 'comments' : comments} 
+    context = {'blog': blog, 'comments': comments}
     return render(request, 'blog.html', context)
 
-def blogComment(request, blogId):
+
+def comments(request, blogId):
     if request.method == "POST":
         comment = request.POST.get("comment")
-        name = request.POST.get("name")
-        email = request.POST.get("email")
-        blogSno = request.POST.get("blogSno")
-        blog = Blog.objects.get(sno=blogSno)
-        
-        comment = BlogComment(comment = comment, name=name, email=email, blog=blog)
-        comment.save()    
-    
-    return render("/blog") # to be changed
+        mail = request.POST.get("mail")
+        blog = Blog.objects.get(id=blogId)
+
+        comment = BlogComment(comment=comment, blog=blog, mail=mail)
+        comment.save()
+
+    return redirect("blog", blogId=blogId)  # to be changed
